79174dbd8ca1cc3eaa2f7f167638858845dd108d
Blayze Millward <blayze.millward@googlemail.com>
2020-02-29 23:00:46 +0000

    KillableSprite now can move faster in the y direction

--- KillableSprite.java
+++ KillableSprite.java
@@ -4,6 +4,9 @@ import blayzeTechUtils.math.NVector;
 import java.util.*;
 
 public class KillableSprite extends Sprite implements Actionable {
+
+	public double yMovement = 0;
+
 	public KillableSprite(double x, double y, String path, int columns, int rows, float fps)
 	{
 		super(x, y, path, columns, rows, fps);
@@ -13,5 +16,12 @@ public class KillableSprite extends Sprite implements Actionable {
 	public void action(GameEnvironment env)
 	{
 		this.markedForDeath = true;
+	}
+
+	@Override
+	public void update(long time)
+	{
+		this.setY(this.getY() + yMovement);
+		super.update();
 	}
 }
